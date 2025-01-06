87a178ca3674735c58f647be579f130205cc5d34
Padmavati-123 <162453633+Padmavati-123@users.noreply.github.com>
2025-01-06 21:16:29 +0530

    Update app.js

--- Truth/app.js
+++ Truth/app.js
@@ -1,11 +1,13 @@
 document.getElementById("invisible-button").addEventListener("click", function() {
-    console.log(`"L dp irxqg zkhuh pbvwhulhv xqirog,
-L fdq jurz zkhq wkh wuxwk lv xqwrog.
-L dp vlohqw, bhw L vshdn wr wkrvh zkr vhhn,
-Wkurxjk revhuydwlrq, pb vhfuhwv brx’oo shhn.
-Zkdw dp L?"
+    console.log(`"Hidden within lies a secret, encoded as in the days of Caesar; shift each letter thrice to uncover the truth."
+        
+    "L dp irxqg zkhuh pbvwhulhv xqirog,
+     L fdq jurz zkhq wkh wuxwk lv xqwrog.
+     L dp vlohqw, bhw L vshdn wr wkrvh zkr vhhn,
+     Wkurxjk revhuydwlrq, pb vhfuhwv brx’oo shhn.
+     Zkdw dp L?"
 
-kwwsv://gulyh.jrrjoh.frp/iloh/g/11RwHtV-ZGNNdYvz0euEx4RQk0ox1iPFo/ylhz?xvs=gulyh_olqn`);
+     kwwsv://gulyh.jrrjoh.frp/iloh/g/1y9dvW8vxetsZyL3cKtCnpOTCU1SyJO0w/ylhz?xvs=gulyh_olqn`);
 });
 
 const buttons = document.querySelectorAll(".invisible-button1");
